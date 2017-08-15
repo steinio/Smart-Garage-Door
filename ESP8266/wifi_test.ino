9ceaf93712aa530bdccf65df36b133fae4b7e096
steinio <stein.inge.oien@gmail.com>
2017-08-15 13:48:15 +0200

    Update wifi_test.ino

--- ESP8266/wifi_test.ino
+++ ESP8266/wifi_test.ino
@@ -1,17 +1,17 @@
-// Import required libraries
+// Import ESP8266 wifi library
 #include "ESP8266WiFi.h"
 
 // WiFi parameters
-const char* ssid = "Windows 10 Update";
-const char* password = "DefNotAVirus";
+char* ssid = "Windows 10 Update";
+char* pwd = "DefNotAVirus";
 
 void setup(void)
 { 
-// Start Serial
+// Start Serial Communication
 Serial.begin(115200);
 
 // Connect to WiFi
-WiFi.begin(ssid, password);
+WiFi.begin(ssid, pwd);
 while (WiFi.status() != WL_CONNECTED) {
 delay(500);
 Serial.print(".");
